b6290953441454570fa613f9d8002bdb3c115f88
Bendroid30 <123538578+Bendroid30@users.noreply.github.com>
2023-10-18 11:27:31 +0200

    Agregado Vocales

--- PROGRA SERVER - copia/U1/Vocales/src/vocales/Ej7_UtilidadesFicheros.java
+++ PROGRA SERVER - copia/U1/Vocales/src/vocales/Ej7_UtilidadesFicheros.java
@@ -1,11 +1,6 @@
 package vocales;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Ej7_UtilidadesFicheros {
