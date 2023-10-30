e074841c25835af55d094638725a7494a8b33c64
Bendroid30 <123538578+Bendroid30@users.noreply.github.com>
2023-10-30 09:45:37 +0100

    EjemplosTema2 (TallerPracticas1)

--- PROGRA SERVER - copia/U2/Ejemplos_Tema2/src/ejercicios_modificados/tallerPracticas1/Alumno.java
+++ PROGRA SERVER - copia/U2/Ejemplos_Tema2/src/ejercicios_modificados/tallerPracticas1/Alumno.java
@@ -4,14 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Alumno implements  Runnable{
 
-    Herramienta herramientas=new Herramienta(10);
-
-    public Alumno(Herramienta herramientas) {
-        this.herramientas = herramientas;
-    }
 
     @Override
     public void run() {
-        int herramienta1= herramientas.getHerramientasLista().get(ThreadLocalRandom.current().nextInt());
+
     }
 }
--- PROGRA SERVER - copia/U2/Ejemplos_Tema2/src/ejercicios_modificados/tallerPracticas1/MainTaller.java
+++ PROGRA SERVER - copia/U2/Ejemplos_Tema2/src/ejercicios_modificados/tallerPracticas1/MainTaller.java
@@ -2,6 +2,6 @@ package ejercicios_modificados.tallerPracticas1;
 
 public class MainTaller {
     public static void main(String[] args) {
-        Herramienta bancoHerramietnas=new Herramienta(10);
+        BancoHerramientas bancoHerramientas=new BancoHerramientas(10);
     }
 }
